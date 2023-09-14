afeefd990b9535edd285abcebe0d5c6ea195f3fd
dep <1649743146@qq.com>
2023-09-14 10:00:11 +0800

    feat: 删除链表的节点

---
+++ 剑指offer-牛客/17删除链表的节点.js
@@ -0,0 +1,31 @@
+function ListNode(x) {
+    this.val = x;
+    this.next = null;
+}
+
+// 这种删除
+function deleteNode(head, val) {
+    // write code here
+    if(head.val == val) return head.next;
+
+    let p = head;
+    let q = head.next;
+    while(q != null) {
+      if(q.val == val) {
+        if(q.next) {
+            p.next = q.next
+        } else {
+            p.next = null
+        }
+        break;
+      }
+      p = q;
+      q = q.next;
+    }
+    
+}
+
+function deleteNode2(head, val) {
+    if(head.val == val) return head.next;
+
+}
